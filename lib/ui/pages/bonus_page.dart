00711b64a15445ff089bf9bacfb8b86933968c2f
muhammadshiva <shivamuhammad07@gmail.com>
2022-09-20 11:56:33 +0700

    slicing bonus vbagian 2

--- lib/ui/pages/bonus_page.dart
+++ lib/ui/pages/bonus_page.dart
@@ -94,6 +94,65 @@ class BonusPage extends StatelessWidget {
       );
     }
 
+    Widget title() {
+      return Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: 80,
+            ),
+            child: Text(
+              'Big Bonus 🎉',
+              style: blackTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: semiBold,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget subTitle() {
+      return Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Text(
+          'We give you early credit so that\nyou can buy a flight ticket',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+
+    Widget startButton() {
+      return Container(
+        width: 220,
+        height: 55,
+        margin: EdgeInsets.only(top: 50),
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            backgroundColor: kPrimaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                defaultRadius,
+              ),
+            ),
+          ),
+          child: Text(
+            'Start Fly Now',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: kBackgroundColor,
@@ -102,6 +161,9 @@ class BonusPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               bonusCard(),
+              title(),
+              subTitle(),
+              startButton(),
             ],
           ),
         ),
